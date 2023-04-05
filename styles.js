692291a81002ac9ad4e9a3e0eb0393890d6ed531
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-05 21:53:25 +0200

    extend global styles

--- styles.js
+++ styles.js
@@ -11,4 +11,57 @@ export default createGlobalStyle`
     margin: 0;
     font-family: system-ui;
   }
+
+  body {
+    line-height: 1.5;
+    --webkit-font-smoothing: antialiased;
+    font-family: 'System.ui';
+    font-weight: 400;
+    overflow-x: hidden;
+    }
+
+    main {
+      display: flex;
+    }
+    img, 
+    picture, 
+    video, 
+    canvas, 
+    svg {
+      display: block;
+      max-width: 100%;
+    }
+
+    input, 
+    button, 
+    textarea, 
+    select {
+      font: inherit;
+    }
+    
+    p, 
+    h1, 
+    h2, 
+    h3, 
+    h4, 
+    h5, 
+    h6 {
+      overflow-wrap: break-word;
+    }
+
+    p {
+    hyphens: auto;
+    }
+
+    #root, #__next {
+  isolation: isolate;
+}
+  ul {
+    list-style: none;
+  }
+  a {
+    display: flex;
+    text-decoration: none;
+  
+}
 `;
