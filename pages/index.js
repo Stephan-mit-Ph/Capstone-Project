cfa63f1e7f58595627ad7c71e831c4fa2cf29404
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-05 21:56:10 +0200

    add homepage and data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+            <Image
+                src="/../public/images/iphone-13-pro-max-graphit 1.png"
+                alt="iPhone 13 Collection"
+                width={300}
+                height={300}
+            />
+            <Link href="/categories">All Categories</Link>
+            <p>
+                Looking for high-quality refurbished Apple products at
+                unbeatable prices? Look no further than our e-commerce store! We
+                offer a wide selection of top-of-the-line Apple
+                products,including iPhones, iPads, MacBooks, and more, all
+                professionally refurbished to like-new condition and backed by
+                our quality guarantee.
+            </p>
+        </>
   );
 }
