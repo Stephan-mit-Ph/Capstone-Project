a2331a7ac0d129ca0306fcf0c7d0421fa188e7fd
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-06 21:49:40 +0200

    add data and pages

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -1,12 +1,11 @@
-import HomeLink from "../../components/HomeLink";
+import BackToLink from "../../components/HomeLink";
 import CategoryList from "../../components/CategoryList/CategoryList";
 
 export default function CategoryPage() {
   return (
-    <>
-        <HomeLink />
-      <h2>Category</h2>
+    <div>
+        <BackToLink />
       <CategoryList />
-    </>
+    </div>
   );
 }
