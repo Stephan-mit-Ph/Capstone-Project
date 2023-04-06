60daa257566f51f1fe6a2a5324cf016685c8f3ea
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-06 13:25:49 +0200

    fix Link

--- components/HomeLink.js
+++ components/HomeLink.js
@@ -1,15 +1,20 @@
+import styled from "styled-components";
 import Link from "next/link";
 import ChevronLeft from '/components/Icons/ChevronLeft';
 
+const StyledLink = styled.a`
+    display: flex;
+    padding-top: 10%;
+`;
 
 
 export default function HomeLink() {
     return (
         <>
-        <Link href="/">
+        <StyledLink href="/">
             <ChevronLeft />
             Home
-        </Link>
+        </StyledLink>
         </>
     );
 }
