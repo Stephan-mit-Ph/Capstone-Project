25f741421c483ea130dfebebe48b6a94e51fd003
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-11 13:37:36 +0200

    edit detail page

--- pages/categories/[slug]/index.js
+++ pages/categories/[slug]/index.js
@@ -1,5 +1,6 @@
 import BackToLink from "../../../components/BackToLink";
 import CategoryList from "../../../components/CategoryList/CategoryList";
+import { useRouter } from "next/router";
 
 export default function CategoryPage() {
   return (
