0f68e89c865cb34dd5752e21c6db9ea70e7a5803
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-19 13:41:57 +0200

    edit alt prop

--- src/components/ProductPreview/index.js
+++ src/components/ProductPreview/index.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { PreviewContainer, Thumbnail, PreviewImage, ThumbnailWrapper } from './ProductPreview.styled'
 
+
+
 const ProductPreview = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(images[0])
 
@@ -11,13 +13,14 @@ const ProductPreview = ({ images }) => {
 
   return (
     <PreviewContainer>
-      <PreviewImage src={selectedImage} width={300} height={300} />
+      <PreviewImage src={selectedImage} width={300} height={300} alt='product-preview' />
       <ThumbnailWrapper>
-        {images.map((image) => (
-          <Thumbnail key={image.id} onClick={() => handleThumbnailClick(image)}>
-            <Image src={image} width={80} height={80} alt="product-preview" />
-          </Thumbnail>
-        ))}
+      {images.map((image) => (
+        <Thumbnail key={image.id} onClick={() => handleThumbnailClick(image)}>
+          <Image src={image} width={80} height={80} alt='product-preview' />
+        </Thumbnail>
+        
+      ))}
       </ThumbnailWrapper>
     </PreviewContainer>
   )
