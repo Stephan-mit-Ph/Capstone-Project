c5ea74cbd2ac937464816e8d408532a291c11d16
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-22 23:08:01 +0200

    conver icon components

--- src/components/Icons/ChevronLeft.js
+++ src/components/Icons/ChevronLeft.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 const SvgChevronLeft = ({ title, titleId, ...props }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" fill="none" viewBox="0 0 24 24" aria-labelledby={titleId} {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path
-      fill="#1C274C"
+      fill="#000"
       fillRule="evenodd"
-      d="M15.488 4.43a.75.75 0 0 1 .081 1.058L9.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081Z"
+      d="M15.325 4.62a.5.5 0 0 1 .055.705L9.659 12l5.72 6.675a.5.5 0 1 1-.759.65l-6-7a.5.5 0 0 1 0-.65l6-7a.5.5 0 0 1 .705-.055Z"
       clipRule="evenodd"
     />
   </svg>
--- src/components/Icons/ChevronRight.js
+++ src/components/Icons/ChevronRight.js
@@ -1,13 +1,13 @@
 import * as React from 'react'
-const SvgChevronDown = ({ title, titleId, ...props }) => (
+const SvgChevronRight = ({ title, titleId, ...props }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path
       fill="#000"
       fillRule="evenodd"
-      d="M4.62 8.675a.5.5 0 0 1 .705-.055L12 14.342l6.675-5.722a.5.5 0 1 1 .65.76l-7 6a.5.5 0 0 1-.65 0l-7-6a.5.5 0 0 1-.055-.705Z"
+      d="M8.675 4.62a.5.5 0 0 1 .705.055l6 7a.5.5 0 0 1 0 .65l-6 7a.5.5 0 1 1-.76-.65L14.342 12 8.62 5.325a.5.5 0 0 1 .055-.705Z"
       clipRule="evenodd"
     />
   </svg>
 )
-export default SvgChevronDown
+export default SvgChevronRight
