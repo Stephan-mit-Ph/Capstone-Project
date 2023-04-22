89cb7a51f162492926ccbbe9bc5bf71b6d255c28
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-22 10:27:13 +0200

    fix typo

--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -5,13 +5,12 @@ import { StyledSubTitle } from '../ProductDetails/ProductDetails.styled'
 import { StyledProductInfo, StyledCartItem, StyledCartList, StyledProductDetails, StyledProductName } from './ShoppingCart.styled'
 
 function getTotalSum(cart) {
-    return cart.reduce((total, item) => total + item.sum, 0);
-  }
-  
+  return cart.reduce((total, item) => total + item.sum, 0)
+}
 
 export default function ShoppingCart() {
   const cart = useShopStore((state) => state.cart)
-  const totalSum = getTotalSum(cart);
+  const totalSum = getTotalSum(cart)
   return (
     <>
       <StyledSubTitle>My Cart</StyledSubTitle>
@@ -21,16 +20,14 @@ export default function ShoppingCart() {
             <StyledCartItem key={item.id}>
               <Image src={item.image[0].path} alt={item.name} width={100} height={100} />
               <StyledProductDetails>
-              <StyledProductName>{item.name}</StyledProductName>
-              <StyledProductInfo>Qty: {item.quantity}</StyledProductInfo>
-              <StyledProductInfo>Price: {formatCurrency(item.price)}</StyledProductInfo>
-              <StyledProductInfo>{formatCurrency(item.sum)}</StyledProductInfo>
+                <StyledProductName>{item.name}</StyledProductName>
+                <StyledProductInfo>Qty: {item.quantity}</StyledProductInfo>
+                <StyledProductInfo>Price: {formatCurrency(item.price)}</StyledProductInfo>
+                <StyledProductInfo>{formatCurrency(item.sum)}</StyledProductInfo>
               </StyledProductDetails>
-           
             </StyledCartItem>
-          
           ))}
-             <StyeledProductInfo>Total: {formatCurrency(totalSum)}</StyeledProductInfo>
+        <StyledProductInfo>Total: {formatCurrency(totalSum)}</StyledProductInfo>
       </StyledCartList>
     </>
   )
