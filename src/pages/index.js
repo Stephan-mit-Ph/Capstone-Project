fec9949d692bacf74a488a8af645eba354e46c95
Stephan Niendorf <stephan@stephanmitph.com>
2023-04-28 19:39:13 +0200

    add head

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import HomePreview from '../components/HomePreview'
 import HeroImage from '../components/HeroImage'
 import HeroBanner from '../../public/images/hero-banner.png'
+import Head from 'next/head'
 
 export default function HomePage() {
   return (
     <>
+    <Head>
+      <title>Home | Refurbished Apple Products</title>
+      <meta name="description" content="Welcome to our Online-Shop. Looking for high-quality refurbished Apple products at unbeatable prices? Look no further than our e-commerce store! We offer a wide selection of top-of-the-line Apple products, including iPhones, iPads, MacBooks, and more, all professionally refurbished to like-new condition and backed by our quality guarantee." />
+    </Head>
       <HeroImage src={HeroBanner} alt="Herobanner" />
       <HomePreview />
     </>
