ecafac6adcab80e2282e852893a508e284d27dd5
Stephan Niendorf <stephan@stephanmitph.com>
2023-05-01 03:03:01 +0200

    move head component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,16 @@ import Layout from '../components/Layout/index.js'
 import { categories } from '../data/categories'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <Head>
+        <title>SEC:Tech</title>
+        <meta name="decription" content="Capstone Project by Stephan Niendorf @neuefische GmbH -School and Pool for digital Talents" />
+      </Head>
       <Layout>
         <Component {...pageProps} categories={categories} />
         <ToastContainer />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
 import HomePreview from '../components/HomePreview'
 import HeroImage from '../components/HeroImage'
 import HeroBanner from '../../public/images/hero-banner.png'
-import Head from 'next/head'
 
 export default function HomePage() {
   return (
     <>
-      <Head>
-        <title>SEC:Tech</title>
-        <meta name="decription" content="Capstone Project by Stephan Niendorf @neuefische GmbH -School and Pool for digital Talents" />
-      </Head>
       <HeroImage src={HeroBanner} alt="Herobanner" />
       <HomePreview />
     </>
