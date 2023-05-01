8e5ae19c65148efdf1b98653bc5c59145db2bfec
Stephan Niendorf <stephan@stephanmitph.com>
2023-05-01 02:28:01 +0200

    edit next config lang

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en-US', 'de'],
+    defaultLocale: 'en-US',
+  },
   compiler: {
     styledComponents: true,
   },
+
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+      use: ['@svgr/webpack'],
+    })
 
-    return config;
+    return config
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
