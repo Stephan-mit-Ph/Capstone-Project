61d62e6ee533c287e5828f39b60d19704e9ebb89
Stephan Niendorf <stephan@stephanmitph.com>
2023-05-01 03:13:58 +0200

    edit head tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,17 +3,11 @@ import Layout from '../components/Layout/index.js'
 import { categories } from '../data/categories'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Head>
-        <title>SEC:Tech E-Commerce App</title>
-        <meta name="decription" content="Capstone Project by Stephan Niendorf @neuefische GmbH -School and Pool for digital Talents.
-        E-Commerce Shop Application" />
-      </Head>
       <Layout>
         <Component {...pageProps} categories={categories} />
         <ToastContainer />
